a8834100fc6086c37878085f560432803a1d2302
Yotl <yramirezjr@cps.edu>
2023-04-19 09:36:07 -0500

    update

--- neural.py
+++ neural.py
@@ -168,7 +168,7 @@ class NeuralNet:
         learning_rate: float = 0.5,
         momentum_factor: float = 0.1,
         iters: int = 1000,
-        print_interval: int = 10,
+        print_interval: int = 100,
     ) -> None:
         """Carries out a training cycle on the neural net
 
