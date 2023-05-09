4e71fe26023ad25bc32687bb0fb538addfdf831b
Yotl <yramirezjr@cps.edu>
2023-05-09 09:42:00 -0500

    Update

--- data.py
+++ data.py
@@ -10,15 +10,15 @@ def parse_line(line: str) -> Tuple[List[float], List[float]]:
     Returns:
         tuple of input list and output list
     """
-    tokens = line.split(",")
+    tokens = line.split(',')
     # print(tokens)
-    out = int(tokens[0])
-    # print(out)
-    output = [0 if out == 1 else 0.5 if out == 2 else 1]
-    # print(output)
+    out = (tokens[24]).strip()
+    print(out)
+    output = [0 if out == "neutral or dissatisfied" else 1]
+    print(output)
 
-    inpt = [float(x) for x in tokens[1:]]
-    # print(inpt)
+    inpt = [float(x) for x in tokens[7:24]]
+    print(inpt)
     return (inpt, output)
 
 
@@ -46,8 +46,17 @@ def normalize(data: List[Tuple[List[float], List[float]]]):
         for j in range(len(data[i][0])):
             data[i][0][j] = (data[i][0][j] - leasts[j]) / (mosts[j] - leasts[j])
     return data
-with open("train[1].csv", "r") as f:
-    training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
+with open("train.csv", "r") as f:
+    training_data = [parse_line(line) for line in f.readlines() if len(line) > 1]
 td = normalize(training_data)
 for line in training_data:
      print(line)
+train_data, test_data = train_test_split(td, test_size=.8)
+print(len(train_data))
+print(len(test_data))
+for line in td:
+    print(line)
+nn = NeuralNet(17, 1, 1)
+nn.train(train_data, learning_rate=.25)
+for i in nn.test_with_expected(test_data):
+    print(f"desired: {i[1]}, actual: {i[2]}")
